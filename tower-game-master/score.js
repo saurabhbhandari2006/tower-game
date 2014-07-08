2e06361c2965d623c6f8d8023f050072509ebd9e
sandeep80 <sandeep@ptotem.com>
2014-07-08 13:54:47 +0530

    tower game more changes

--- tower-game-master/score.js
+++ tower-game-master/score.js
@@ -35,20 +35,39 @@ $(document).ready(function () {
     var blue_val = 50; // for blue
     var green_val = 50; // for green
     var red, blue, green, attack;
-    var tower_health = 60;
+    var tower_health = 50;
     var cal_health_tower;
     var r40_50 = false;
     var r30_40 = false;
     var r20_30 = false;
     var r10_20 = false;
+    var card_type;
 
     $('#red_score').text(red_val);
     $('#blue_score').text(blue_val);
     $('#green_score').text(green_val);
+    $('#tower_health').text(tower_health);
+
     user_turn = true;
     console.log("user_turn  on load :- " + user_turn);
     $(".cards").click(function (event) {
+        card_type = $(this).find('img').attr("data-val-type");
 
+        switch(card_type) {
+            case "build":
+                call_build();
+                break;
+            case "attack":
+                call_attack();
+                break;
+            case "resource":
+                call_resource();
+                break;
+            default:
+                console.log("do nothing")
+        }
+
+        console.log();
         user_turn = true;
         comp_turn = false;
         //console.log("user_turn  on click of card :- " + user_turn);
@@ -56,16 +75,18 @@ $(document).ready(function () {
         red = $(this).find('img').attr("data-val-red");
         blue = $(this).find('img').attr("data-val-blue");
         green = $(this).find('img').attr("data-val-green");
-        attack = $(this).find('img').attr("data-val-attack")
+        attack = $(this).find('img').attr("data-val-attack");
 
 
         var calRed = $('#red_score').text() - parseInt(red);
         var calBlue = $('#blue_score').text() - parseInt(blue);
         var calGreen = $('#green_score').text() - parseInt(green);
+        var cal_health_tower = $('#tower_health').text() - attack;
+        $('#tower_health').text(cal_health_tower);
+        var calculated_th = $('#tower_health').text();
 
-        cal_health_tower= tower_health- attack;
-console.log(cal_health_tower);
-        //console.log(calRed)
+        //console.log(calRed);
+
         if( (parseInt(calRed)<0) ){
             event.preventDefault();
             alert("not enough resources")
@@ -76,23 +97,20 @@ console.log(cal_health_tower);
             $("#green_score").text(calGreen);
         }
 
-//        if (r40_50==false){
-//            if (between(cal_health_tower, 39 , 50)) {
-//                console.log("do nothing");
-
         if (r40_50==false){
-            if (between(cal_health_tower, 39 , 50)) {
+            if (between(calculated_th, 40 , 50)) {
                 console.log("do nothing");
             }
             else{
-                console.log("dec");
-                decrease_tower_height();
+                console.log("top tower damaged");
+                $('#tower_container2').find('#t_block_5').attr('src','images/right/Layer1.png');
+                //decrease_tower_height();
                 r40_50=true;
             }
         }
 
-        if (r30_40==false && r40_50==true){
-            if (between(cal_health_tower, 29, 40)) {
+        if ((r30_40==false) && (r40_50==true)){
+            if (between(calculated_th, 30, 40)) {
                 console.log("do nothing");
             }
             else{
@@ -102,8 +120,8 @@ console.log(cal_health_tower);
             }
         }
 
-        if (r20_30==false && r30_40==true && r40_50==true){
-            if (between(cal_health_tower, 19, 30)) {
+        if ((r20_30==false) && (r30_40==true) && (r40_50==true)){
+            if (between(calculated_th, 20, 30)) {
                 console.log("do nothing");
             }
             else{
@@ -113,8 +131,8 @@ console.log(cal_health_tower);
             }
         }
 
-        if (r10_20==false && r20_30==true && r30_40==true && r40_50==true){
-            if (between(cal_health_tower, 10, 20)) {
+        if ((r10_20==false) && (r20_30==true) && (r30_40==true) && (r40_50==true)){
+            if (between(calculated_th, 10, 20)) {
                 console.log("do nothing");
             }
             else{
@@ -137,6 +155,18 @@ console.log(cal_health_tower);
     });
 
 });
+
+function call_attack(){
+    console.log("in attack fn");
+}
+
+function call_build(){
+    console.log("in build fn");
+}
+
+function call_resource(){
+    console.log("in resource fn");
+}
 
 function decrease_tower_height(){
     $('#tower_container2').animate({height: '-=50'}, 500);
